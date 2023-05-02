2807c285be9f926632b74c0bc8cb1be813042dba
Dragon-Wu1 <69114084+Dragon-Wu1@users.noreply.github.com>
2023-05-02 15:45:45 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -554,8 +554,7 @@ def LoggedIn_Clicked(userName, password):
     df2 = cursor.fetchone()
     df = int(df2[2])
     # mark down
-    st.write(type(password))
-    password = int(password)
+    password = float(password)
     if df2 is not None and df == password:
         st.session_state['loggedIn'] = True
         if 'UserName' not in st.session_state:
